bebf83397919159bad453e24135d149f0f42e535
Rafee Memon <rafeememon@gmail.com>
2023-01-07 23:54:13 -0700

    WIP 8

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # nanodbc-java-test
 
+To set up the PowerShell build environment, use the following commands:
+
+```
+Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+
+# For x86
+Enter-VsDevShell -VsInstallPath "C:\Program Files\Microsoft Visual Studio\2022\Community" -DevCmdArguments '-arch=x86'
+
+# For x64
+Enter-VsDevShell -VsInstallPath "C:\Program Files\Microsoft Visual Studio\2022\Community" -DevCmdArguments '-arch=x64'
+
+```
+
+(reference: https://stackoverflow.com/a/69961784)
+
+```
+
 To run using the natively built library, add something like the following to VM arguments:
 
 ```
--- src/main/java/io/nanodbc/RafeeTest.java
+++ src/main/java/io/nanodbc/RafeeTest.java
@@ -11,12 +11,15 @@ public class RafeeTest {
     public static void main(String[] args) {
         Connection c = new Connection();
         c.connect(str("DSN=EquipMgrX5E_64"));
-        Statement s = new Statement(c, str("SELECT UnitID, UnitDesc FROM UnitTitles"));
+        // SELECT UnitID, UnitDesc FROM UnitTitles WHERE UnitDesc = 'µA'
+        Statement s = new Statement(c, str("SELECT UnitID, UnitDesc FROM UnitTitles WHERE UnitID = 5"));
         Result r = s.execute();
         while (r.next()) {
             int id = r.getInt(str("UnitID"));
+            // String desc = r.getString(str("UnitDesc"));
             CharPointer desc = r.getString(str("UnitDesc"));
-            System.out.println(id + " " + desc.getStringChars());
+            // System.out.println(id + " " + desc.getString());
+            System.out.println(id + " " + desc.getString());
         }
     }
 
